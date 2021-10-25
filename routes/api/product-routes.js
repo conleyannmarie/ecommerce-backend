b47a4067854d0dcab41d5d18c15352302ada95ab
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-24 19:04:57 -0600

    added category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({ 
+    include: [Product, Category, Tag],
+  })
+  .then((categoryOutput) => {
+    res.json(categoryOutput);
+  })
+  .catch((error) => {
+    res.status(500).json(error);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbPost => {
+    res.json(dbPost);
+  })
+  .catch((error) => {
+    res.status(500).json(error);
+  });
 });
 
 // create new product
