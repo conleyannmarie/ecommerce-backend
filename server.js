94398591967755430f4c02525d75f5bb28a15722
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-11-12 17:20:07 -0700

    fixed stuff up

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelizeConnection = require('./config/connection.js');
+const sequelize = require('./config/connection.js');
+const mysql = require('mysql2');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelizeConnection.sync({ force: false}).then(() => {
+sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => {
 
     console.log(`App listening on port ${PORT}!`);
